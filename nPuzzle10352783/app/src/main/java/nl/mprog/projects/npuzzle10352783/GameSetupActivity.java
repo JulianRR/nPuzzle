dae778c0bde9a48b1f15cd2bacb220128106051c
Julian Ruger <julianruger@wcw-stud-145-109-15-93.wireless.uva.nl>
2014-09-25 15:19:16 +0200

    working on choosing image from gallery

--- nPuzzle10352783/app/src/main/java/nl/mprog/projects/npuzzle10352783/GameSetupActivity.java
+++ nPuzzle10352783/app/src/main/java/nl/mprog/projects/npuzzle10352783/GameSetupActivity.java
@@ -5,13 +5,21 @@ import nl.mprog.projects.npuzzle10352783.util.SystemUiHider;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 /**
@@ -29,9 +37,11 @@ public class GameSetupActivity extends Activity {
     private static final int MEDIUM = 4;
     private static final int HARD = 5;
 
-    Button easy, medium, hard;
+    Button easy, medium, hard, customImg;
     ImageButton im1, im2, im3;
     Intent intent;
+    ImageView customImage;
+    Uri imageUri;
 
 
 
@@ -46,10 +56,13 @@ public class GameSetupActivity extends Activity {
         easy = (Button) findViewById(R.id.easy_button);
         medium = (Button) findViewById(R.id.medium_button);
         hard = (Button) findViewById(R.id.hard_button);
+        customImg = (Button) findViewById(R.id.custom_button);
 
         im1 = (ImageButton) findViewById(R.id.imageButton);
         im2 = (ImageButton) findViewById(R.id.imageButton2);
         im3 = (ImageButton) findViewById(R.id.imageButton3);
+
+        customImage = (ImageView) findViewById(R.id.customImageView);
 
 
         intent = new Intent(this, GamePlayActivity.class);
@@ -105,6 +118,30 @@ public class GameSetupActivity extends Activity {
             }
         });
 
+        customImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pickImgIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(pickImgIntent, 1);
+
+                imageUri = pickImgIntent.getData();
+
+
+                Bitmap bitmap;
+
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                    customImage.setImageBitmap(bitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+
 
 
     }
@@ -114,5 +151,26 @@ public class GameSetupActivity extends Activity {
         super.onPostCreate(savedInstanceState);
 
     }
+
+    /** * Photo Selection result */
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (RESULT_OK == resultCode) {
+//            Uri imageUri = intent.getData();
+//            Bitmap bitmap;
+//            try {
+//                bitmap = MediaStore.Images.Media.getBitmap(
+//                        getContentResolver(), imageUri);
+//                customImage.setImageBitmap(bitmap);
+//            } catch (FileNotFoundException e) {
+//                // TODO Auto-generated catch block
+//                e.printStackTrace();
+//            } catch (IOException e) {
+//                // TODO Auto-generated catch block
+//                e.printStackTrace();
+//            }
+//
+//        }
+//    }
+
 
 }
